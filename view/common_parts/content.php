e9eeae1401dd95a35cc616ba2e142fb180b4c405
Rankod <akronder@gmail.com>
2018-01-24 12:50:52 +0100

    Utworzon wzorzec MVC oraz widok strony głównej

---
+++ view/common_parts/content.php
@@ -0,0 +1,4 @@
+        <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+        <h1 class="display-4">Firma Kocyjan i Wspólnicy przedstawia</h1>
+        <h1>Panie i Panowie runda: <span class="badge badge-secondary">TU będzie coś z PHP</span></h1> 
+        <button type="button" class="btn btn-dark lead">RockNRoll- polacz z php</button>
